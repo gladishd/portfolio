480f1fe9f50ddef7178939b4bed1717a3abc155b
gladishd <gladish.dean@gmail.com>
2022-06-02 13:53:29 -0500

    Committing changes modeled after the tutorial https://levelup.gitconnected.com/build-an-awesome-developer-portfolio-website-using-react-667abd7bab4d by Trey Huffine, founder of skilled.dev and gitconnected.com. With his passion for helping developers grow their careers, we can therefore make the claims that * The import in src/index.js should be import ./index.scss * MobileNav ./styles should be ./style (or style.js be styles.js). Content is from @carbon/react, potentially. * On src/components/Layout/styles.js, instead of import { Content } from 'carbon-components-react/lib/components/UIShell' or import { Content } from '@carbon/react', import { Content } from 'carbon-components-react'. * Actually, carbon-components-react natively has the reference to Content which can be destructured from within. * In MobileNav.js, import { User, Code, Portfolio, Education } from '@carbon/icons-react' instead of import { User32, Code32, Portfolio32, Education32 } from '@carbon/icons-react'. * In src/components/MobileNav/styles.js, import { Button } from 'carbon-components-react' instead of import Button from 'carbon-components-react/lib/components/Button'. * Similarly, in src/components/Sidebar/styles.js, import { SideNav } from 'carbon-components-react' (instead of import { SideNav } from 'carbon-components-react/lib/components/UIShell'). * On src/components/Sidebar/Sidebar.js import { SideNavItems, SideNavLink } from 'carbon-components-react' instead of import { SideNavItems, SideNavLink } from 'carbon-components-react/lib/components/UIShell'. * On src/components/UserHeader/UserHeader.js import { ArrowRight } from '@carbon/icons-react' instead of import { ArrowRight16 } from '@carbon/icons-react'. Renders <ArrowRight />. * For the imports for carbon themes on src/index.scss which provides the theme, removes the style with the undefined variable $theme--g100 and the potential fix,        @import '@carbon/themes/scss/themes';        @import 'carbon-components/scss/globals/scss/styles.scss';        // Use the gray 100 theme $theme: $theme--g100;        @include theme();

---
+++ src/components/Sidebar/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+import { SideNav } from 'carbon-components-react'
+
+export const StyledSideNav = styled(SideNav)`
+  @media (max-width: 640px) {
+    display: none;
+  }
+`
